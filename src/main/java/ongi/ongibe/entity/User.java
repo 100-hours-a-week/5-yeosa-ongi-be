9097e049943c5e85f3976fbf7948a914a16114ec
Yehun <yhyh4420@naver.com>
2025-04-28 17:33:19 +0900

    feat: User, OAuthToken 엔티티 추가

--- src/main/java/ongi/ongibe/entity/User.java
+++ src/main/java/ongi/ongibe/entity/User.java
@@ -1,5 +1,6 @@
 package ongi.ongibe.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,8 +8,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,19 +33,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OAuthToken> oAuthTokens;
+
     @Column(nullable = false, length = 20)
     private String provider;
 
     @Column(nullable = false, length = 50)
     private String providerId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 30)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 512)
     private String profileImage;
 
-    @Column
+    @Column(length = 320)
     private String email;
 
     @CreationTimestamp
