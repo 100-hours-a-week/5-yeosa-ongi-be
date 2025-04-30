f3f7627187ef5d7558819ea24efe7ac5382cdb56
Yehun <yhyh4420@naver.com>
2025-04-30 20:03:40 +0900

    rename: 폴더구조 변경

--- src/main/java/ongi/ongibe/domain/album/entity/Album.java
+++ src/main/java/ongi/ongibe/domain/album/entity/Album.java
@@ -17,7 +17,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ongi.ongibe.domain.Picture;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
