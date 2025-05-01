ea48469aa80122435294fcb4a26e4f71dece5379
Yehun <yhyh4420@naver.com>
2025-05-01 17:24:03 +0900

    refactor: BaseApiResponse 적용

--- src/main/java/ongi/ongibe/global/s3/dto/PresignedUrlRequestDTO.java
+++ src/main/java/ongi/ongibe/global/s3/dto/PresignedUrlRequestDTO.java
@@ -2,6 +2,6 @@ package ongi.ongibe.global.s3.dto;
 
 import java.util.List;
 
-public record PresignedUrlRequest(List<PictureInfo> pictures) {
+public record PresignedUrlRequestDTO(List<PictureInfo> pictures) {
     public record PictureInfo(String pictureName, String pictureType) {}
 }
