867ed9f77b9bfaa4e2fa626d62dca6cfbec90800
Yehun <yhyh4420@naver.com>
2025-05-02 15:18:58 +0900

    refactor: place 도메인 패키지 별도 생성

--- src/main/java/ongi/ongibe/domain/album/service/AlbumProcessService.java
+++ src/main/java/ongi/ongibe/domain/album/service/AlbumProcessService.java
@@ -2,10 +2,14 @@ package ongi.ongibe.domain.album.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import ongi.ongibe.domain.album.dto.KakaoAddressDTO;
 import ongi.ongibe.domain.album.entity.Album;
 import ongi.ongibe.domain.album.entity.Picture;
+import ongi.ongibe.domain.place.entity.Place;
 import ongi.ongibe.domain.album.repository.AlbumRepository;
 import ongi.ongibe.domain.album.repository.PictureRepository;
+import ongi.ongibe.domain.album.repository.PlaceRepository;
+import ongi.ongibe.domain.place.service.PlaceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -19,6 +23,8 @@ public class AlbumProcessService {
     private final AlbumRepository albumRepository;
     private final PictureRepository pictureRepository;
     private final S3MetadataService s3MetadataService;
+    private final KakaoMapService kakaoMapService;
+    private final PlaceService placeService;
 
     @Async
     @Transactional
@@ -29,5 +35,17 @@ public class AlbumProcessService {
                 );
         List<Picture> pictures = album.getPictures();
 
+        for (Picture p:pictures){
+            var gps = s3MetadataService.extractGPS(p.getPictureURL());
+            p.setLatitude(gps.lat());
+            p.setLongitude(gps.lon());
+
+            var address = kakaoMapService.reverseGeocode(gps.lat(), gps.lon());
+            Place place = placeService.findOrCreate(address);
+            p.setPlace(place);
+        }
+        pictureRepository.saveAll(pictures);
+        album.setPictures(pictures);
+        albumRepository.save(album);
     }
 }
