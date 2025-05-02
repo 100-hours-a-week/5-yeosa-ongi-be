d39f7a6d5f8a9ee6ba8fa1f68a1d64db119cb466
Yehun <yhyh4420@naver.com>
2025-05-03 01:11:01 +0900

    refactor: BaseApiResponse에 정적메서드 추가

--- src/main/java/ongi/ongibe/common/BaseApiResponse.java
+++ src/main/java/ongi/ongibe/common/BaseApiResponse.java
@@ -11,4 +11,12 @@ public class BaseApiResponse<T> {
     private String code;
     private String message;
     private T data;
+
+    public static <T> BaseApiResponse<T> success(String code, String message, T data) {
+        return BaseApiResponse.<T>builder()
+                .code(code)
+                .message(message)
+                .data(data)
+                .build();
+    }
 }
