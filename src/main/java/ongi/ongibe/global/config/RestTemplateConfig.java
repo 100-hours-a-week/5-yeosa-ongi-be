ea7b9e1cbaf745bba54b0096d5309afd8ddc4d60
Yehun <yhyh4420@naver.com>
2025-05-02 22:15:40 +0900

    fix: rest template가 application/cbor -> application/json 반환 수정

--- src/main/java/ongi/ongibe/global/config/RestTemplateConfig.java
+++ src/main/java/ongi/ongibe/global/config/RestTemplateConfig.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -17,6 +19,7 @@ public class RestTemplateConfig {
         RestTemplate restTemplate = new RestTemplate();
 
         List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        messageConverters.add(new MappingJackson2HttpMessageConverter());
         messageConverters.add(new MappingJackson2CborHttpMessageConverter());
         messageConverters.add(new FormHttpMessageConverter());
 
