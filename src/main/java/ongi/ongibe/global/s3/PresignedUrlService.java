0ce7ab4bbc5f5a51d3e08deefd2031c65a08bf5e
Yehun <yhyh4420@naver.com>
2025-05-02 13:15:39 +0900

    refactor: 빌더패턴 메서드 분리

--- src/main/java/ongi/ongibe/global/s3/PresignedUrlService.java
+++ src/main/java/ongi/ongibe/global/s3/PresignedUrlService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import ongi.ongibe.common.BaseApiResponse;
 import ongi.ongibe.global.s3.dto.PresignedUrlRequestDTO;
+import ongi.ongibe.global.s3.dto.PresignedUrlRequestDTO.PictureInfo;
 import ongi.ongibe.global.s3.dto.PresignedUrlResponseDTO;
 import ongi.ongibe.global.s3.dto.PresignedUrlResponseDTO.PresignedFile;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,16 +32,9 @@ public class PresignedUrlService {
                 .map(picture -> {
                     String key = picture.pictureName();
 
-                    PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                            .bucket(bucket)
-                            .key(key)
-                            .contentType(picture.pictureType())
-                            .build();
+                    PutObjectRequest putObjectRequest = getObjectRequest(picture, key);
 
-                    PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
-                            .putObjectRequest(putObjectRequest)
-                            .signatureDuration(Duration.ofMinutes(10)) // todo : 몇분 설정할지 상의
-                            .build();
+                    PutObjectPresignRequest presignRequest = getPresignRequest(putObjectRequest);
 
                     URL presignedUrl = presigner.presignPutObject(presignRequest).url();
 
@@ -56,6 +50,22 @@ public class PresignedUrlService {
                 .code("PRESIGNED_URL_SUCCESS")
                 .message("presigned-url 발급 성공")
                 .data(new PresignedUrlResponseDTO(result))
+                .build();
+    }
+
+    private PutObjectPresignRequest getPresignRequest(
+            PutObjectRequest putObjectRequest) {
+        return PutObjectPresignRequest.builder()
+                .putObjectRequest(putObjectRequest)
+                .signatureDuration(Duration.ofMinutes(10)) // todo : 몇분 설정할지 상의
+                .build();
+    }
+
+    private PutObjectRequest getObjectRequest(PictureInfo picture, String key) {
+        return PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(picture.pictureType())
                 .build();
     }
 }
