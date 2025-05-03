ccea05af3eb126a3f26419ea74b6264eda52e2ca
Yehun <yhyh4420@naver.com>
2025-05-04 02:22:37 +0900

    feat: 임시로 CORS 정책 개방

--- src/main/java/ongi/ongibe/global/security/config/SecurityConfig.java
+++ src/main/java/ongi/ongibe/global/security/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.addAllowedOriginPattern("*");
+        //configuration.setAllowedOrigins(List.of("http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
