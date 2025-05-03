1c55eb9d90f384dc74456c6eb5d76586369694e3
Yehun <yhyh4420@naver.com>
2025-05-03 20:16:42 +0900

    feat: 별도 스레드풀 생성

--- src/main/java/ongi/ongibe/global/config/AsyncConfig.java
+++ src/main/java/ongi/ongibe/global/config/AsyncConfig.java
@@ -1,9 +1,35 @@
 package ongi.ongibe.global.config;
 
+import java.util.concurrent.Executor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+@Slf4j
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            log.error("[Async 예외] 메서드: {}, 메시지: {}", method.getName(), ex.getMessage(), ex);
+        };
+    }
+
+    @Override
+    @Bean(name = "asyncExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // 기본 스레드 수
+        executor.setMaxPoolSize(20); // 최대 스레드 수
+        executor.setQueueCapacity(500); // 큐 대기 용량
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
 }
