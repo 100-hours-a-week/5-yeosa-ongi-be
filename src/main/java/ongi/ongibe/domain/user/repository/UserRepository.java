75180b48a5288f6a8ab90ee13d69525277ed82e5
Yehun <yhyh4420@naver.com>
2025-05-06 11:18:06 +0900

    feat: 일별 사진 개수 조회 JPQL

--- src/main/java/ongi/ongibe/domain/user/repository/UserRepository.java
+++ src/main/java/ongi/ongibe/domain/user/repository/UserRepository.java
@@ -1,12 +1,28 @@
 package ongi.ongibe.domain.user.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import ongi.ongibe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByProviderId(String providerId);
+
+    @Query("""
+        select DATE(p.createdAt), count(p)
+        from Picture p
+        where p.user.id = :userId
+        and p.createdAt between :startDate and :endDate
+        group by DATE(p.createdAt)
+        order by DATE(p.createdAt)
+    """)
+    List<Object[]> countPicturesByDate(@Param("userId") Long userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate);
 }
