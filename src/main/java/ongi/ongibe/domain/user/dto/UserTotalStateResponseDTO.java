a5ecf39c7273e7fe7fb31d299e8600d989cac883
Yehun <yhyh4420@naver.com>
2025-05-06 16:08:40 +0900

    feat: 유저 도메인 DTO swagger 스키마 정의

--- src/main/java/ongi/ongibe/domain/user/dto/UserTotalStateResponseDTO.java
+++ src/main/java/ongi/ongibe/domain/user/dto/UserTotalStateResponseDTO.java
@@ -1,14 +1,15 @@
 package ongi.ongibe.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record UserTotalStateResponseDTO(
-        List<PictureCoordinate> pictureCoordinates,
-        int albumCount,
-        int placeCount
+        @Schema(description = "사진의 좌표 목록") List<PictureCoordinate> pictureCoordinates,
+        @Schema(description = "총 앨범 수") int albumCount,
+        @Schema(description = "총 방문 장소 수") int placeCount
 ) {
     public record PictureCoordinate(
-            double latitude,
-            double longitude
+            @Schema(description = "위도") double latitude,
+            @Schema(description = "경도") double longitude
     ) {}
 }
