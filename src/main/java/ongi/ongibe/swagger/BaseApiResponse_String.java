1f11db82056c976c1834701ccb9fee0393a63cdb
Yehun <yhyh4420@naver.com>
2025-05-06 16:54:00 +0900

    docs: swagger 래퍼클래스

---
+++ src/main/java/ongi/ongibe/swagger/BaseApiResponse_String.java
@@ -0,0 +1,10 @@
+package ongi.ongibe.swagger;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import ongi.ongibe.common.BaseApiResponse;
+
+/**
+ * BaseApiResponse<String> 의 Swagger 표현용 클래스
+ */
+@Schema(name = "BaseApiResponse_String", description = "문자열 데이터 응답")
+public class BaseApiResponse_String extends BaseApiResponse<String> {}
