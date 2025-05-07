f9e5e0e3dde940f3a02132d25888c391683bb48c
Yehun <yhyh4420@naver.com>
2025-05-07 19:01:36 +0900

    feat: 메타데이터 추출 로직 구현

--- src/main/java/ongi/ongibe/domain/album/service/S3MetadataService.java
+++ src/main/java/ongi/ongibe/domain/album/service/S3MetadataService.java
@@ -20,18 +20,34 @@ public class S3MetadataService {
     private String bucket;
 
     public GPSCoordinateDTO extractGPS(String pictureUrl) {
-        String key = pictureUrl.substring(pictureUrl.indexOf(".amazonaws.com/") + 15);
+        String key;
+        String actualBucket;
+
+        if (pictureUrl.startsWith("s3://")) {
+            // s3://bucket-name/key 형식일 경우
+            String withoutPrefix = pictureUrl.substring(5); // remove "s3://"
+            int slashIndex = withoutPrefix.indexOf("/");
+            actualBucket = withoutPrefix.substring(0, slashIndex);
+            key = withoutPrefix.substring(slashIndex + 1);
+        } else if (pictureUrl.contains(".amazonaws.com/")) {
+            // https 형식일 경우
+            actualBucket = bucket;
+            key = pictureUrl.substring(pictureUrl.indexOf(".amazonaws.com/") + 15);
+        } else {
+            throw new IllegalArgumentException("지원하지 않는 pictureUrl 형식: " + pictureUrl);
+        }
+
         try (var s3Object = s3Client.getObject(
-                GetObjectRequest.builder().bucket(bucket).key(key).build())) {
+                GetObjectRequest.builder().bucket(actualBucket).key(key).build())) {
             Metadata metadata = ImageMetadataReader.readMetadata(s3Object);
             GpsDirectory gps = metadata.getFirstDirectoryOfType(GpsDirectory.class);
             if (gps != null && gps.getGeoLocation() != null) {
                 var geo = gps.getGeoLocation();
                 return new GPSCoordinateDTO(geo.getLatitude(), geo.getLongitude());
-            }
+            } else {return new GPSCoordinateDTO(null, null);}
         } catch (Exception e) {
             throw new RuntimeException("GPS 추출 실패: " + key, e);
         }
-        throw new RuntimeException("GPS 정보 없음: " + key);
     }
+
 }
