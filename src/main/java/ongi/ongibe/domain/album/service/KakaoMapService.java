dea51ce3c547dcc0aba5ea0e320c7f09ff3e109c
Yehun <yhyh4420@naver.com>
2025-05-16 20:07:50 +0900

    feat: 카카오 좌표변환 api 호출 WebClient 변경

--- src/main/java/ongi/ongibe/domain/album/service/KakaoMapService.java
+++ src/main/java/ongi/ongibe/domain/album/service/KakaoMapService.java
@@ -1,5 +1,6 @@
 package ongi.ongibe.domain.album.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ongi.ongibe.domain.album.dto.KakaoAddressDTO;
 import ongi.ongibe.domain.album.dto.KakaoReverseGeocodeDTO;
@@ -7,37 +8,52 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class KakaoMapService {
 
     @Value("${spring.kakao.auth.client}")
     private String kakaoApiKey;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     public KakaoAddressDTO reverseGeocode(double lat, double lon) {
-        String url = String.format("https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=%f&y=%f", lon, lat);
+        String url = String.format(
+                "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=%f&y=%f", lon, lat);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "KakaoAK " + kakaoApiKey);
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Authorization", "KakaoAK " + kakaoApiKey);
 
-        var response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers),
-                KakaoReverseGeocodeDTO.class);
+        var response = webClient.get()
+                .uri(url)
+                .header("Authorization", "KakaoAK " + kakaoApiKey)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(KakaoReverseGeocodeDTO.class)
+                .block();
 
-        log.info("response body : {}", response.getBody().toString());
+        if (response == null) {
+            throw new RuntimeException("카카오 응답이 비정상적입니다");
+        }
 
-        var address = response.getBody().documents().stream()
-                .filter(doc -> doc.region_type().equals("B"))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("주소 없음"));
+            log.info("response body : {}", response.documents());
 
-        return new KakaoAddressDTO(
-                address.region_1depth_name(),
-                address.region_2depth_name(),
-                address.region_3depth_name());
-    }
+            var address = response.documents().stream()
+                    .filter(doc -> doc.region_type().equals("B"))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("주소 없음"));
+
+            return new KakaoAddressDTO(
+                    address.region_1depth_name(),
+                    address.region_2depth_name(),
+                    address.region_3depth_name());
+        }
+
 }
