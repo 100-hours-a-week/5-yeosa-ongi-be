9cfbc79737f89cc42d3cb02ff8f2d0ae49e0dae2
Yehun <yhyh4420@naver.com>
2025-05-16 22:05:40 +0900

    feat: webclientconfig 커스텀

--- src/main/java/ongi/ongibe/global/config/WebClientConfig.java
+++ src/main/java/ongi/ongibe/global/config/WebClientConfig.java
@@ -1,19 +1,41 @@
 package ongi.ongibe.global.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
+        ConnectionProvider provider = ConnectionProvider.builder("custom")
+                .maxConnections(500)                         // 동시에 열 수 있는 최대 커넥션 수
+                .pendingAcquireMaxCount(1000)                // 요청 대기열
+                .pendingAcquireTimeout(Duration.ofSeconds(30)) // 대기 timeout
+                .maxIdleTime(Duration.ofSeconds(30))         // idle 연결 유지 시간
+                .maxLifeTime(Duration.ofMinutes(2))          // 최대 연결 유지 시간
+                .build();
+
+        HttpClient httpClient = HttpClient.create(provider)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(10, TimeUnit.SECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(10, TimeUnit.SECONDS))
+                );
+
         return WebClient.builder()
-                .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)) // 필요시 조절
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024)) // 필요 시 메모리 제한 조정
                 .build();
     }
-
 }
