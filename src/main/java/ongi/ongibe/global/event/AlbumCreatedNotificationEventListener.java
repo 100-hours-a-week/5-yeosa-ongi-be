76b808a83e14e23516872ae2a720d895f54f50fd
Yehun <yhyh4420@naver.com>
2025-05-18 01:09:48 +0900

    refactor: 안쓰는 라이브러리 정리

--- src/main/java/ongi/ongibe/global/event/AlbumCreatedNotificationEventListener.java
+++ src/main/java/ongi/ongibe/global/event/AlbumCreatedNotificationEventListener.java
@@ -1,25 +1,14 @@
 package ongi.ongibe.global.event;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ongi.ongibe.domain.ai.event.AlbumAiCreateNotificationEvent;
-import ongi.ongibe.domain.album.entity.Album;
-import ongi.ongibe.domain.album.entity.UserAlbum;
-import ongi.ongibe.domain.album.repository.AlbumRepository;
-import ongi.ongibe.domain.album.repository.UserAlbumRepository;
 import ongi.ongibe.domain.notification.NotificationService;
-import ongi.ongibe.domain.notification.NotificationType;
-import ongi.ongibe.domain.notification.entity.Notification;
 import ongi.ongibe.domain.notification.event.AlbumCreatedNotificationEvent;
-import ongi.ongibe.domain.user.entity.User;
-import ongi.ongibe.domain.user.repository.UserRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
-import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Component
