2fb88a524e5ef85ff8a73e9a3e739e6de2caeea6
Yehun <yhyh4420@naver.com>
2025-05-23 11:12:26 +0900

    fix : enum타입 수정용 스크립트 작성

---
+++ src/main/resources/db/migration/V8__edit_enum_type_notification.sql
@@ -0,0 +1,12 @@
+ALTER TABLE notification
+    MODIFY COLUMN type ENUM(
+    'ALBUM_CREATED',
+    'ALBUM_CREATED_AI',
+    'ALBUM_MEMBER_JOIN',
+    'ALBUM_EXILE',
+    'ALBUM_INVITE',
+    'ALBUM_TRANSFER',
+    'FEED_COMMENT',
+    'FEED_LIKE',
+    'FOLLOW'
+    );
