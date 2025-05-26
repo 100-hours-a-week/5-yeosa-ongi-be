5fed9afd24a1bb44e07af55068297446c1a7bee7
Yehun <yhyh4420@naver.com>
2025-05-26 16:18:39 +0900

    feat: 오타 수정

--- src/main/java/ongi/ongibe/global/eventlistener/AlbumCreatedRetryEventListener.java
+++ src/main/java/ongi/ongibe/global/eventlistener/AlbumCreatedRetryEventListener.java
@@ -4,17 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ongi.ongibe.domain.album.event.AlbumRetryEvent;
 import ongi.ongibe.domain.album.service.AlbumProcessService;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class AlbumCreataedRetryEventListener {
+public class AlbumCreatedRetryEventListener {
 
     private final AlbumProcessService albumProcessService;
 
     @Async
+    @EventListener
     public void handledAlbumCreatedRetry(AlbumRetryEvent event) {
         log.info("retry event received: {}", event.albumId());
         albumProcessService.processAlbumAsync(event.albumId(), event.pictureS3Keys());
