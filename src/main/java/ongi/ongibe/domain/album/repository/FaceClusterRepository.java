e1ca1f046014c414dce30f9b4df7c6b92fbd8ddd
Yehun <yhyh4420@naver.com>
2025-05-27 23:53:01 +0900

    feat: 엔티티 매핑 레포지토리 생성

---
+++ src/main/java/ongi/ongibe/domain/album/repository/FaceClusterRepository.java
@@ -0,0 +1,8 @@
+package ongi.ongibe.domain.album.repository;
+
+import ongi.ongibe.domain.album.entity.FaceCluster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FaceClusterRepository extends JpaRepository<FaceCluster, Long> {
+
+}
---
+++ src/main/java/ongi/ongibe/domain/album/repository/PictureFaceClusterRepository.java
@@ -0,0 +1,8 @@
+package ongi.ongibe.domain.album.repository;
+
+import ongi.ongibe.domain.album.entity.PictureFaceCluster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PictureFaceClusterRepository extends JpaRepository<PictureFaceCluster, Long> {
+
+}
