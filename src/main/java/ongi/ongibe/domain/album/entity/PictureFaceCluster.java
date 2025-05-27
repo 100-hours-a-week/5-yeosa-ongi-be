ce64310a78742a2001afd63109e878bb95da849d
Yehun <yhyh4420@naver.com>
2025-05-27 20:42:03 +0900

    feat: faceCluster, pictureFaceCluster 엔티티 적용

---
+++ src/main/java/ongi/ongibe/domain/album/entity/PictureFaceCluster.java
@@ -0,0 +1,36 @@
+package ongi.ongibe.domain.album.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PictureFaceCluster {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "picture_id", nullable = false)
+    private Picture picture;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "face_cluster_id", nullable = false)
+    private FaceCluster faceCluster;
+}
