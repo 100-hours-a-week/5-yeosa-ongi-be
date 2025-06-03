7513aa43c0a193ebf09c58b90f1a851a81dc9f6c
Yehun <yhyh4420@naver.com>
2025-06-03 15:14:56 +0900

    feat: fetch join 적용'

--- src/main/java/ongi/ongibe/domain/album/repository/UserAlbumRepository.java
+++ src/main/java/ongi/ongibe/domain/album/repository/UserAlbumRepository.java
@@ -7,10 +7,21 @@ import ongi.ongibe.domain.album.entity.Album;
 import ongi.ongibe.domain.album.entity.UserAlbum;
 import ongi.ongibe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserAlbumRepository extends JpaRepository<UserAlbum, Long> {
 
-    List<UserAlbum> findAllByUser(User user);
+    @Query("""
+    SELECT DISTINCT ua
+    FROM UserAlbum ua
+    JOIN FETCH ua.album a
+    JOIN FETCH a.thumbnailPicture tp
+    JOIN FETCH a.userAlbums aua
+    JOIN FETCH aua.user u
+    WHERE ua.user = :user AND ua.deletedAt IS NULL
+    """)
+    List<UserAlbum> findAllByUser(@Param("user") User user);
     boolean existsByUserAndAlbum_CreatedAtBefore(User user, LocalDateTime dateTime);
 
     int countByUser(User user);
