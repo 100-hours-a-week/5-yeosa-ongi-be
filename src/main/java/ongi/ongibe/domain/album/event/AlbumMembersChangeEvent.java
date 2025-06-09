26b9a5ac30000f71a618edb5578edbc6efe55846
Yehun <yhyh4420@naver.com>
2025-06-10 07:59:08 +0900

    feat: untracked file 추가

---
+++ src/main/java/ongi/ongibe/domain/album/event/AlbumMembersChangeEvent.java
@@ -0,0 +1,15 @@
+package ongi.ongibe.domain.album.event;
+
+import java.util.List;
+import ongi.ongibe.domain.album.entity.Album;
+import ongi.ongibe.util.DateUtil;
+
+public record AlbumMembersChangeEvent(Long albumId, String yearMonth, List<Long> userIds) implements AlbumChangeEvent {
+    public static AlbumNameChangeEvent from(Album album) {
+        return new AlbumNameChangeEvent(
+                album.getId(),
+                DateUtil.getYearMonth(album.getCreatedAt()),
+                album.getUserAlbums().stream().map(ua -> ua.getUser().getId()).toList()
+        );
+    }
+}
