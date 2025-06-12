9b3ced439faf0b1297e918216248ed5112e583bf
Yehun <yhyh4420@naver.com>
2025-06-12 20:45:52 +0900

    feat: dto 수정

--- src/main/java/ongi/ongibe/domain/user/dto/UserPlaceStatResponseDTO.java
+++ src/main/java/ongi/ongibe/domain/user/dto/UserPlaceStatResponseDTO.java
@@ -7,5 +7,7 @@ public record UserPlaceStatResponseDTO(
         @Schema(description = "시 또는 도") String city,
         @Schema(description = "구") String district,
         @Schema(description = "동") String town,
-        @Schema(description = "해당 지역에서의 태그") List<String> tags
-) {}
+        @Schema(description = "해당 지역에서의 태그와 태그가 기록된 숫자") List<TagCountDTO> tags
+) {
+    public record TagCountDTO(String tag, int count) {}
+}
