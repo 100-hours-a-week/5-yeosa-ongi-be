01738cd8397ff414af69f5b28a08c63632743134
Yehun <yhyh4420@naver.com>
2025-06-19 21:36:15 +0900

    feat: embedding producer 구현

---
+++ src/main/java/ongi/ongibe/domain/ai/producer/AiEmbeddingProducer.java
@@ -0,0 +1,28 @@
+package ongi.ongibe.domain.ai.producer;
+
+import com.github.f4b6a3.ulid.Ulid;
+import com.github.f4b6a3.ulid.UlidCreator;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ongi.ongibe.domain.ai.dto.AiImageRequestDTO;
+import ongi.ongibe.domain.ai.dto.KafkaDTOWrapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AiEmbeddingProducer {
+
+    @Value("${kafka.topic.request.embedding}")
+    private String requestTopic;
+    private final AiKafkaProducer aiKafkaProducer;
+
+    public void requestEmbeddings(Long albumId, Long userId, List<String> s3keys) {
+        log.info("Sending embeddings request to topic '{}'", requestTopic);
+        String taskId = UlidCreator.getUlid().toString();
+        KafkaDTOWrapper<AiImageRequestDTO> dto = new KafkaDTOWrapper<>(taskId, albumId, new AiImageRequestDTO(s3keys));
+        aiKafkaProducer.send(requestTopic, userId.toString(), dto);
+    }
+}
