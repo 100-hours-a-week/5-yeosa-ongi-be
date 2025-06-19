0559a20e81446ca2ecee5559c32069c3ba9e1053
Yehun <yhyh4420@naver.com>
2025-06-19 17:28:31 +0900

    feat: 테스트 후 테스트파일 삭제

--- src/main/java/ongi/ongibe/global/security/config/SecurityConfig.java
+++ src/main/java/ongi/ongibe/global/security/config/SecurityConfig.java
@@ -36,8 +36,7 @@ public class SecurityConfig {
             "/api/auth/**",
             "/actuator/**",
             "/sentry/**",
-            "/api/testauth",
-            "/kafka/**"
+            "/api/testauth"
     };
 
 
