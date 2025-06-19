64d143cc7c7aa33f39ca107b1fc8122d4a60fced
Yehun <yhyh4420@naver.com>
2025-06-19 21:33:47 +0900

    feat: key 받는 로직으로 수정

--- src/main/java/ongi/ongibe/domain/ai/producer/AiKafkaProducer.java
+++ src/main/java/ongi/ongibe/domain/ai/producer/AiKafkaProducer.java
@@ -10,7 +10,7 @@ public class AiKafkaProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void send(String topic, Object payload) {
-        kafkaTemplate.send(topic, payload);
+    public void send(String topic, String key, Object payload) {
+        kafkaTemplate.send(topic, key, payload);
     }
 }
