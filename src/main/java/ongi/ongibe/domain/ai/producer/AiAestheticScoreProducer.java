40fdb58635ebf1031ce2fd943022bea7286fe419
Yehun <yhyh4420@naver.com>
2025-06-20 15:09:19 +0900

    feat: aesthetic score producer 구현

---
+++ src/main/java/ongi/ongibe/domain/ai/producer/AiAestheticScoreProducer.java
@@ -0,0 +1,36 @@
+package ongi.ongibe.domain.ai.producer;
+
+import com.github.f4b6a3.ulid.UlidCreator;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ongi.ongibe.domain.ai.aiInterface.AiAestheticServiceInterface;
+import ongi.ongibe.domain.ai.dto.AiAestheticScoreRequestDTO;
+import ongi.ongibe.domain.ai.dto.KafkaDTOWrapper;
+import ongi.ongibe.domain.album.entity.Picture;
+import ongi.ongibe.domain.album.repository.PictureRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AiAestheticScoreProducer implements AiAestheticServiceInterface {
+
+    @Value("${kafka.topic.request.aesthetic}")
+    private String requestTopic;
+    private final AiKafkaProducer aiKafkaProducer;
+    private final PictureRepository pictureRepository;
+
+    @Override
+    public void requestAestheticScores(Long albumId, Long userId, List<String> s3keys) {
+        String taskId = "aesthetic-" + UlidCreator.getUlid().toString();
+
+        List<Picture> pictures = pictureRepository.findAllByS3KeyIn(s3keys);
+        AiAestheticScoreRequestDTO request = AiAestheticScoreRequestDTO.from(pictures);
+        KafkaDTOWrapper<AiAestheticScoreRequestDTO> dto = new KafkaDTOWrapper<>(taskId, albumId, request);
+
+        aiKafkaProducer.send(requestTopic, userId.toString(), dto);
+    }
+}
