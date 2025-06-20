999a77ffedeaeb0056b531f1482abb123d95b431
Yehun <yhyh4420@naver.com>
2025-06-20 13:30:51 +0900

    feat: quality 측정 프로듀서 구현

--- src/main/java/ongi/ongibe/domain/ai/producer/AiShakeProducer.java
+++ src/main/java/ongi/ongibe/domain/ai/producer/AiShakeProducer.java
@@ -1,4 +1,27 @@
-package ongi.ongibe.domain.ai.kafka;
+package ongi.ongibe.domain.ai.producer;
 
+import com.github.f4b6a3.ulid.UlidCreator;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ongi.ongibe.domain.ai.dto.AiImageRequestDTO;
+import ongi.ongibe.domain.ai.dto.KafkaDTOWrapper;
+import ongi.ongibe.domain.ai.service.AiClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class AiShakeProducer {
+
+    @Value("${kafka.topic.request.quality}")
+    private String requestTopic;
+    private final AiKafkaProducer aiKafkaProducer;
+
+    public void requestShaky(Long albumId, Long userId, List<String> s3keys) {
+        String taskId = "quality-" + UlidCreator.getUlid().toString();
+        KafkaDTOWrapper<AiImageRequestDTO> dto = new KafkaDTOWrapper<>(taskId, albumId, new AiImageRequestDTO(s3keys));
+        aiKafkaProducer.send(requestTopic, userId.toString(), dto);
+    }
 }
