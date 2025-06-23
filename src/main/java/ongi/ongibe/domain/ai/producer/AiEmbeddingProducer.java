caf3a60fa8194b22e7cf1c551ba94613c94f6bb0
Yehun <yhyh4420@naver.com>
2025-06-23 17:17:15 +0900

    feat: key 생성 전략 수정

--- src/main/java/ongi/ongibe/domain/ai/producer/AiEmbeddingProducer.java
+++ src/main/java/ongi/ongibe/domain/ai/producer/AiEmbeddingProducer.java
@@ -4,6 +4,7 @@ import com.github.f4b6a3.ulid.UlidCreator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ongi.ongibe.domain.ai.AiStep;
 import ongi.ongibe.domain.ai.aiInterface.AiEmbeddingServiceInterface;
 import ongi.ongibe.domain.ai.dto.AiImageRequestDTO;
 import ongi.ongibe.domain.ai.dto.KafkaRequestDTOWrapper;
@@ -21,8 +22,9 @@ public class AiEmbeddingProducer implements AiEmbeddingServiceInterface {
 
     @Override
     public void requestEmbeddings(Long albumId, Long userId, List<String> s3keys) {
-        String taskId = "embedding-" + UlidCreator.getUlid().toString();
+        String topic = AiStep.EMBEDDING.toString();
+        String taskId = topic + "-" + UlidCreator.getUlid().toString();
         KafkaRequestDTOWrapper<AiImageRequestDTO> dto = new KafkaRequestDTOWrapper<>(taskId, albumId, new AiImageRequestDTO(s3keys));
-        aiKafkaProducer.send(requestTopic, userId.toString(), dto);
+        aiKafkaProducer.send(requestTopic, topic + userId, dto);
     }
 }
