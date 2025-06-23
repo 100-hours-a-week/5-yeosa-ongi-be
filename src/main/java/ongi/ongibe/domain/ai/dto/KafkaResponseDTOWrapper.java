e6d545e97db0edcfd4fe25c9c55687ad76c8f902
Yehun <yhyh4420@naver.com>
2025-06-23 16:47:33 +0900

    feat: status code 필드 추가

--- src/main/java/ongi/ongibe/domain/ai/dto/KafkaResponseDTOWrapper.java
+++ src/main/java/ongi/ongibe/domain/ai/dto/KafkaResponseDTOWrapper.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record KafkaResponseDTOWrapper<T>(
         @Schema(description = "작업 식별자") String taskId,
         @Schema(description = "앨범 아이디") Long albumId,
+        @Schema(description = "상태 코드") int statusCode,
         @JsonUnwrapped
         @Schema(description = "기존 사용하던 ai 통신용 DTO") T body
 ) {}
