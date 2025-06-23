d01d6edbd95c98f0306acf2f2cb4ccceb7813191
Yehun <yhyh4420@naver.com>
2025-06-23 23:42:56 +0900

    feat: kafka album service embedding 시작 로직

--- src/main/java/ongi/ongibe/domain/ai/kafka/AiKafkaAlbumService.java
+++ src/main/java/ongi/ongibe/domain/ai/kafka/AiKafkaAlbumService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ongi.ongibe.domain.ai.aiInterface.AiAlbumServiceInterface;
+import ongi.ongibe.domain.ai.entity.AiTaskStatus;
+import ongi.ongibe.domain.ai.producer.AiEmbeddingProducer;
+import ongi.ongibe.domain.ai.repository.AiTaskStatusRepository;
 import ongi.ongibe.domain.album.entity.Album;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,8 +17,11 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AiKafkaAlbumService implements AiAlbumServiceInterface {
 
+    private final AiEmbeddingProducer embeddingProducer;
+
     @Override
     public void process(Album album, Long userId, List<String> s3keys) {
-
+        Long albumId = album.getId();
+        embeddingProducer.requestEmbeddings(albumId, userId, s3keys);
     }
 }
