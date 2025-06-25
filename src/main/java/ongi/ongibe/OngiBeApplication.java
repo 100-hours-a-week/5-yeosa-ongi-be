2707621b8a214a4fb5acd85177b66198990ee556
Yehun <yhyh4420@naver.com>
2025-06-25 16:57:01 +0900

    feat: enable kafka 어노테이션 추가

--- src/main/java/ongi/ongibe/OngiBeApplication.java
+++ src/main/java/ongi/ongibe/OngiBeApplication.java
@@ -2,11 +2,13 @@ package ongi.ongibe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @EnableScheduling
+@EnableKafka
 @SpringBootApplication
 public class OngiBeApplication {
 
