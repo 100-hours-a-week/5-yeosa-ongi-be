02c5bb746b1f50a633e33273f8d5a3d5b1c1bee0
Yehun <yhyh4420@naver.com>
2025-06-27 16:07:02 +0900

    feat: comment repository 구현

--- src/main/java/ongi/ongibe/domain/album/repository/CommentRepository.java
+++ src/main/java/ongi/ongibe/domain/album/repository/CommentRepository.java
@@ -1,5 +1,7 @@
 package ongi.ongibe.domain.album.repository;
 
+import java.util.List;
+import ongi.ongibe.domain.album.entity.Album;
 import ongi.ongibe.domain.album.entity.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comments, Long> {
 
+    List<Comments> findAllByAlbum(Album album);
 }
