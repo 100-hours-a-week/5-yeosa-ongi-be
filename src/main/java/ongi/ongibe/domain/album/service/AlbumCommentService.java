01b63c964d483e59c409587010dfa08b85561c67
Yehun <yhyh4420@naver.com>
2025-06-27 16:07:58 +0900

    feat: read comments 서비스클래스 구현

--- src/main/java/ongi/ongibe/domain/album/service/AlbumCommentService.java
+++ src/main/java/ongi/ongibe/domain/album/service/AlbumCommentService.java
@@ -1,15 +1,36 @@
 package ongi.ongibe.domain.album.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import ongi.ongibe.common.BaseApiResponse;
+import ongi.ongibe.domain.album.dto.AlbumCommentResponseDTO;
+import ongi.ongibe.domain.album.entity.Album;
+import ongi.ongibe.domain.album.entity.Comments;
+import ongi.ongibe.domain.album.exception.AlbumException;
+import ongi.ongibe.domain.album.repository.AlbumRepository;
 import ongi.ongibe.domain.album.repository.CommentRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 @RequiredArgsConstructor
 public class AlbumCommentService {
 
     private final CommentRepository commentRepository;
+    private final AlbumRepository albumRepository;
 
-
-
+    @Transactional(readOnly = true)
+    public BaseApiResponse<List<AlbumCommentResponseDTO>> readComments(@PathVariable("albumId") Long albumId) {
+        Album album = albumRepository.findById(albumId).orElseThrow(
+                () -> new AlbumException(HttpStatus.NOT_FOUND, "앨범을 찾을 수 없습니다.")
+        );
+        List<Comments> comments = commentRepository.findAllByAlbum(album);
+        List<AlbumCommentResponseDTO> dtoList = comments.stream()
+                .map(AlbumCommentResponseDTO::from).toList();
+        return BaseApiResponse.success("COMMENT_READ_SUCCESS", "댓글 조회에 성공했습니다.",  dtoList);
+    }
 }
