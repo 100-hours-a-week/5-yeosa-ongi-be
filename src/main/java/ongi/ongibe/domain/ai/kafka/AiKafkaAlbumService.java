242d0326da7f644f7c9253112398b21bdec17a7e
Yehun <yhyh4420@naver.com>
2025-07-01 16:48:12 +0900

    feat: kafka primary 적용

--- src/main/java/ongi/ongibe/domain/ai/kafka/AiKafkaAlbumService.java
+++ src/main/java/ongi/ongibe/domain/ai/kafka/AiKafkaAlbumService.java
@@ -12,7 +12,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-@Qualifier("kafka")
+@Primary
+//@Qualifier("kafka")
 @Service
 @RequiredArgsConstructor
 @Slf4j
