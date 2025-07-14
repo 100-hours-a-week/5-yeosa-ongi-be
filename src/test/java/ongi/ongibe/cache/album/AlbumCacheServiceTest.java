a5f0f100c052534a10758fe467c165a63868eaf5
Yehun <yhyh4420@naver.com>
2025-07-14 16:20:56 +0900

    style: 불필요 라이브러리 임포트 제거

--- src/test/java/ongi/ongibe/cache/album/AlbumCacheServiceTest.java
+++ src/test/java/ongi/ongibe/cache/album/AlbumCacheServiceTest.java
@@ -2,7 +2,6 @@ package ongi.ongibe.cache.album;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
